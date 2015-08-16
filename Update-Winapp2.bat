9f3c91b604c5926793cbed227faaaef46c77475f
Joe <coffee4joe@yahoo.com>
2015-08-15 21:25:14 -0400

    Fix issue with it saving to C:\Windows\System32
    added full path %ProgramFiles%\CCleaner\. Should be able to run as admin from any location (Desktop etc)

--- Update-Winapp2.bat
+++ Update-Winapp2.bat
@@ -1,25 +1,25 @@
 @echo off
-cd C:\Program Files\CCleaner
 echo Checking For Updates!
-findstr " Version:" winapp2.ini > oldversion.txt
-powershell -command $web=New-Object Net.WebClient ; $web.DownloadString('http://www.winapp2.com/winapp2.ini') | find " Version:" > version.txt
+findstr " Version:" "%ProgramFiles%"\CCleaner\winapp2.ini > "%ProgramFiles%"\CCleaner\oldversion.txt
+powershell -command $web=New-Object Net.WebClient ; $web.DownloadString('http://www.winapp2.com/winapp2.ini') | find " Version:" > "%ProgramFiles%"\CCleaner\version.txt
 
-fc oldversion.txt version.txt /lb1 /w > nul
+fc "%ProgramFiles%"\CCleaner\oldversion.txt "%ProgramFiles%"\CCleaner\version.txt /lb1 /w > nul
 if errorlevel 1 goto :error
 
 :next
 echo No New Updates!
-del oldversion.txt version.txt
+del "%ProgramFiles%"\CCleaner\oldversion.txt "%ProgramFiles%"\CCleaner\version.txt
 pause
 exit
 
 :error
 echo Update Is Available!
 echo Would You Like to Update? (ctrl+c to cancel)
-del oldversion.txt version.txt
+del "%ProgramFiles%"\CCleaner\oldversion.txt "%ProgramFiles%"\CCleaner\version.txt
 pause 
-del winapp2-old.ini
-ren winapp2.ini winapp2-old.ini
+del "%ProgramFiles%"\CCleaner\winapp2-old.ini
+ren "%ProgramFiles%"\CCleaner\winapp2.ini "%ProgramFiles%"\CCleaner\winapp2-old.ini
 powershell Invoke-WebRequest http://winapp2.com/winapp2.ini -OutFile winapp2.ini 
+move winapp2.ini "%ProgramFiles%"\CCleaner\
 echo Winapp2.ini Has Been Updated!
 pause
