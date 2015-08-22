1b88a8e41b889642bfc88347bdf195aae2c5bdd3
Joe <coffee4joe@yahoo.com>
2015-08-22 10:56:10 -0400

    Update Update-Winapp2.bat

--- Update-Winapp2.bat
+++ Update-Winapp2.bat
@@ -18,7 +18,7 @@ echo Would You Like to Update? (ctrl+c to cancel)
 del "%ProgramFiles%"\CCleaner\oldversion.txt "%ProgramFiles%"\CCleaner\version.txt
 pause 
 del "%ProgramFiles%"\CCleaner\winapp2-old.ini
-ren "%ProgramFiles%\CCleaner\WINAPP2.INI" winapp2-old.ini
+ren "%ProgramFiles%\CCleaner\winapp2.ini" winapp2-old.ini
 powershell Invoke-WebRequest http://winapp2.com/winapp2.ini -OutFile winapp2.ini 
 move winapp2.ini "%ProgramFiles%"\CCleaner\
 echo Winapp2.ini Has Been Updated!
