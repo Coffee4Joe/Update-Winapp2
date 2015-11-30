d73ade9c922291b40fefa5fcf25b1cba5671d73c
Joe <coffee4joe@yahoo.com>
2015-11-30 11:09:15 -0500

    added "pushd %~dp0" for portable support.

--- Update-Winapp2.bat
+++ Update-Winapp2.bat
@@ -1,25 +1,27 @@
 @echo off
+echo.CD=%CD%
+pushd %~dp0
+echo.CD=%CD%
 echo Checking For Updates!
-findstr " Version:" "%ProgramFiles%"\CCleaner\winapp2.ini > "%ProgramFiles%"\CCleaner\oldversion.txt
-powershell -command $web=New-Object Net.WebClient ; $web.DownloadString('http://www.winapp2.com/winapp2.ini') | find " Version:" > "%ProgramFiles%"\CCleaner\version.txt
+findstr " Version:" winapp2.ini > oldversion.txt
+powershell -command $web=New-Object Net.WebClient ; $web.DownloadString('http://www.winapp2.com/winapp2.ini') | find " Version:" > version.txt
 
-fc "%ProgramFiles%"\CCleaner\oldversion.txt "%ProgramFiles%"\CCleaner\version.txt /lb1 /w > nul
+fc oldversion.txt version.txt /lb1 /w > nul
 if errorlevel 1 goto :error
 
 :next
 echo No New Updates!
-del "%ProgramFiles%"\CCleaner\oldversion.txt "%ProgramFiles%"\CCleaner\version.txt
+del oldversion.txt version.txt
 pause
 exit
 
 :error
 echo Update Is Available!
 echo Would You Like to Update? (ctrl+c to cancel)
-del "%ProgramFiles%"\CCleaner\oldversion.txt "%ProgramFiles%"\CCleaner\version.txt
+del oldversion.txt version.txt
 pause 
-del "%ProgramFiles%"\CCleaner\winapp2-old.ini
-ren "%ProgramFiles%\CCleaner\winapp2.ini" winapp2-old.ini
+del winapp2-old.ini
+ren winapp2.ini" winapp2-old.ini
 powershell Invoke-WebRequest http://winapp2.com/winapp2.ini -OutFile winapp2.ini 
-move winapp2.ini "%ProgramFiles%"\CCleaner\
-echo Winapp2.ini Has Been Updated!
+echo winapp2.ini Has Been Updated!
 pause
